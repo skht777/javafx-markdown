7bf24efba4322ad1247774815de1feb5815c1070
skht777 <skht@skhtlab.com>
2019-02-24 03:17:06 +0900

    bower is too old technology, it is the time that change to cdn

--- src/resources/web/markdown.js
+++ src/resources/web/markdown.js
@@ -1,6 +1,14 @@
-$(function () {
+window.onload = () => {
     alert("command:ready");
     marked.setOptions({
         langPrefix: ''
     });
-});
+};
+
+window.mark = src => {
+    document.getElementById('result').innerHTML = marked(src);
+    Array.from(document.querySelectorAll('pre code')).forEach(block => {
+        hljs.highlightBlock(block);
+    });
+    return src;
+};
